e4361924b352625501593809dfe8fccd069f93d8
narumir <narumir@narumir.io>
2025-01-07 12:32:12 +0000

    add readme

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,11 @@
 output "narae_content_access_key" {
-  value     = module.cdn_user.s3_uploader_access_key
-  sensitive = true
+  value       = module.cdn_user.s3_uploader_access_key
+  description = "narae-content의 s3 access key"
+  sensitive   = true
 }
 
 output "narae_content_secret_key" {
-  value     = module.cdn_user.s3_uploader_secret_key
-  sensitive = true
+  value       = module.cdn_user.s3_uploader_secret_key
+  description = "narae-content의 s3 secret key"
+  sensitive   = true
 }
--- variables.tf
+++ variables.tf
@@ -1,7 +1,11 @@
 variable "cloudflare_api_token" {
-  type = string
+  type        = string
+  description = "cloudflare에서 제공하는 api token"
+  nullable    = false
 }
 
 variable "cloudflare_zone_id" {
-  type = string
+  type        = string
+  description = "cloudflare에서 제공하는 dns zone id"
+  nullable    = false
 }
