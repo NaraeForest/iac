50ceb43ec8ea65f32799c4d587e32a8e0dff32ac
narumir <narumir@narumir.io>
2025-01-07 11:14:14 +0000

    create cdn user module readme

--- modules/cdn-iam/variables.tf
+++ modules/cdn-iam/variables.tf
@@ -1,11 +1,17 @@
 variable "target_resources" {
-  type = list(string)
+  type        = list(string)
+  description = "AWS S3 arn 리소스 목록"
+  nullable    = false
 }
 
 variable "policy_name" {
-  type = string
+  type        = string
+  description = "생성할 aws iam policy 명"
+  nullable    = false
 }
 
 variable "uploader" {
-  type = string
+  type        = string
+  description = "생성할 aws iam 유저 명"
+  nullable    = false
 }
