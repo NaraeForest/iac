33634c31d7a970feb0342b15617636e889c4c8d0
narumir <narumir@narumir.io>
2025-01-07 10:37:12 +0000

    update acm readme

--- modules/acm/variables.tf
+++ modules/acm/variables.tf
@@ -1,7 +1,9 @@
 variable "domain_name" {
-  type = string
+  type        = string
+  description = "인증서를 발급받을 도메인 주소"
 }
 
 variable "zone_id" {
   type = string
+  description = "cloudflare에서 제공하는 dns zone id"
 }
