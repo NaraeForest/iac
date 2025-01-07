d93c3368af6ade25e2619f53b07b232a69678c32
narumir <narumir@narumir.io>
2025-01-07 10:48:12 +0000

    add cdn modules readme

--- modules/cdn/variables.tf
+++ modules/cdn/variables.tf
@@ -1,20 +1,24 @@
 variable "bucket_name" {
-  type = string
+  type        = string
+  description = "S3 버킷 이름"
 }
 
 variable "zone_id" {
   type        = string
-  description = "Cloudflare's zone id"
+  description = "cloudflare에서 제공하는 dns zone id"
 }
 
 variable "domain_name" {
-  type = string
+  type        = string
+  description = "루트 도메인 주소, 모든 서브 도메인에 cors 적용"
 }
 
 variable "fqdn_domain_name" {
-  type = string
+  type        = string
+  description = "cdn에 연결될 fqdn 도메인 주소"
 }
 
 variable "aws_acm_certificate_arn" {
-  type = string
+  type        = string
+  description = "aws us-east-1에서 발급 받은 acm arn"
 }
